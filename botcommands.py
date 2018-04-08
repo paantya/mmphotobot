885511652b7741fd9bcabbc5c7c609a88bcc1a3b
Nikita Fedorov <nikitafedorovv@gmail.com>
2018-04-08 06:05:57 +0300

    Rename blackout and blur commands

--- botcommands.py
+++ botcommands.py
@@ -2,8 +2,8 @@
 
 SET_HEADING_COMMAND = 'set_heading'
 SET_SUBHEADING_COMMAND = 'set_subheading'
-SET_BLACKOUT_COMMAND = 'set_blackout_intensity'
-SET_BLUR_COMMAND = 'set_blur_radius'
+SET_BLACKOUT_COMMAND = 'set_blackout'
+SET_BLUR_COMMAND = 'set_blur'
 SEND_NEWSLETTER_COMMAND = 'send_newsletter'
 SET_MAILING_LIST_COMMAND = 'set_mailing_list'
 
